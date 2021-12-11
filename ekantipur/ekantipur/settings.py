1c3f6eff9f9cd3e522e5652ce887dedaa4eee430
Sagyam <sagyamthapa32@gmail.com>
2021-12-11 17:25:46 +0545

    setting changed

--- ekantipur/ekantipur/settings.py
+++ ekantipur/ekantipur/settings.py
@@ -82,8 +82,8 @@ AUTOTHROTTLE_START_DELAY = 5
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+#HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
-HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
-HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
